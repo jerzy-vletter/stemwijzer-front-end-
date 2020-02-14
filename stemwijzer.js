ff2e430d92e9fa8e4711ae7a4c680ea52cfd5941
J. vletter <99052114@mydavinci.nl>
2020-02-14 09:10:02 +0100

    (working) buttons, some styling

--- stemwijzer.js
+++ stemwijzer.js
@@ -2,18 +2,31 @@
 var counter = 2;
 
 function start() {
-    var container = document.getElementById("content-container");
+    var content_container = document.getElementById("content_container");
+    var button_container = document.getElementById("button_container");
 
     var h1 = document.createElement("h1");
-    h1.innerHTML = subjects[counter].title;
-
     var p = document.createElement("p");
-    p.innerHTML = subjects[counter].statement;
+    var but1 = document.createElement("button");
+    var but2 = document.createElement("button");
+    var but3 = document.createElement("button");
 
-    container.appendChild(h1);
-    container.appendChild(p);
-   
+    h1.innerHTML = subjects[counter].title;
+    p.innerHTML = subjects[counter].statement;
+    but1.innerHTML = "eens";
+    but2.innerHTML = "geen mening";
+    but3.innerHTML = "oneens";
     
+    but1.setAttribute("class", "btn-answer");
+    but2.setAttribute("class", "btn-answer");
+    but3.setAttribute("class", "btn-answer");
+
+    content_container.appendChild(h1);
+    content_container.appendChild(p);
+    button_container.appendChild(but1);
+    button_container.appendChild(but2);
+    button_container.appendChild(but3);
+
 }
 
 start();
